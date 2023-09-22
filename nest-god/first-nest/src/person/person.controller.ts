6b30cafb50b9fd6b51171d01fca541ed9d5bc948
zhaoxingchen <zhaoxingchen@comen.com>
2023-09-22 15:50:44 +0800

    nest url param传递参数

---
+++ nest-god/first-nest/src/person/person.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+// @Controller('person')
+// export class PersonController {
+//   constructor(private readonly personService: PersonService) {}
+
+//   @Post()
+//   create(@Body() createPersonDto: CreatePersonDto) {
+//     return this.personService.create(createPersonDto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.personService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.personService.findOne(+id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+//     return this.personService.update(+id, updatePersonDto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.personService.remove(+id);
+//   }
+// }
+
+@Controller('api/person')
+export class PersonController {
+  @Get(':id')
+  urlParam(@Param('id') id: string) {
+    return `received:id=${id}`;
+  }
+}
