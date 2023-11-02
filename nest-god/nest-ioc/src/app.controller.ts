bee2544a222ce1fbea1f3da80c490803cd15be7d
zhaoxingchen <zhaoxingchen@comen.com>
2023-11-02 20:36:45 +0800

    nest 自定义provider

--- nest-god/nest-ioc/src/app.controller.ts
+++ nest-god/nest-ioc/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('user1') private readonly user1: { name: string },
+    @Inject('user2') private readonly user2: { name: string },
+    @Inject('user3') private readonly user3: { name: string; desc: string },
+    @Inject('user4') private readonly user4: { name: string; desc: string },
+    @Inject('user5') private readonly user5: { name: string; desc: string },
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log(this.user1);
+    console.log(this.user2);
+    console.log(this.user3);
+    console.log(this.user4);
+    console.log(this.user5);
     return this.appService.getHello();
   }
 }
--- nest-god/nest-ioc/src/app.module.ts
+++ nest-god/nest-ioc/src/app.module.ts
@@ -5,6 +5,48 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'user1',
+      useValue: {
+        name: 'kaho1',
+      },
+    },
+    {
+      provide: 'user2',
+      useFactory() {
+        return {
+          name: 'kaho2',
+        };
+      },
+    },
+    {
+      provide: 'user3',
+      useFactory(user: { name: string }, appService: AppService) {
+        return {
+          name: user.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['user1', AppService],
+    },
+    {
+      provide: 'user4',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+        return {
+          name: 'user4',
+          desc: 'this is user4',
+        };
+      },
+    },
+    {
+      provide: 'user5',
+      useExisting: 'user4',
+    },
+  ],
 })
 export class AppModule {}
