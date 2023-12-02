8bb2c58216f010d135bb2cda782c7e3399fe74dd
zhaoxingchen <zhaoxingchen@comen.com>
2023-12-02 15:44:48 +0800

    nest: 内置 Pipe 和自定义 Pipe

---
+++ nest-god/pipe-test/src/app.controller.ts
@@ -0,0 +1,106 @@
+import {
+  Controller,
+  Get,
+  // HttpException,
+  // HttpStatus,
+  ParseArrayPipe,
+  ParseBoolPipe,
+  ParseFloatPipe,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { Param } from '@nestjs/common';
+import { ParseEnumPipe } from '@nestjs/common';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { DefaultValuePipe } from '@nestjs/common';
+import { AaaPipe } from './aaa.pipe';
+
+enum Ggg {
+  AAA = '111',
+  BBB = '222',
+  CCC = '333',
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('aa')
+  getHello(
+    @Query(
+      'aa',
+      new ParseIntPipe({
+        // errorHttpStatusCode: HttpStatus.NOT_FOUND,
+        // exceptionFactory: (msg) => {
+        //   console.log(msg);
+        //   throw new HttpException('xxx ' + msg, HttpStatus.NOT_IMPLEMENTED);
+        // },
+      }),
+    )
+    aa: number,
+  ): number {
+    return aa + 1;
+  }
+
+  @Get('bb')
+  bb(@Query('bb', ParseFloatPipe) bb: number): number {
+    return bb + 1;
+  }
+
+  @Get('cc')
+  cc(@Query('cc', ParseBoolPipe) cc: boolean): boolean {
+    return cc;
+  }
+
+  @Get('ee')
+  ee(
+    @Query(
+      'ee',
+      new ParseArrayPipe({
+        items: Number,
+      }),
+    )
+    ee: Array<number>,
+  ): number {
+    return ee.reduce((total, item) => total + item, 0);
+  }
+
+  @Get('ff')
+  ff(
+    @Query(
+      'ff',
+      new ParseArrayPipe({
+        separator: '..',
+        optional: true,
+      }),
+    )
+    ff: Array<string>,
+  ): Array<string> {
+    return ff;
+  }
+
+  @Get('gg/:enum')
+  gg(@Param('enum', new ParseEnumPipe(Ggg)) gg: Ggg): Ggg {
+    return gg;
+  }
+
+  @Get('hh/:uuid')
+  hh(@Param('uuid', new ParseUUIDPipe()) uuid: string): string {
+    return uuid;
+  }
+
+  @Get('ii')
+  ii(@Query('ii', new DefaultValuePipe('default value')) ii: string): string {
+    return ii;
+  }
+
+  @Get('jj/:aaa')
+  jj(
+    @Param('aaa', AaaPipe) aaa: string,
+    @Query('bbb', AaaPipe) bbb: string,
+  ): string {
+    console.log(aaa, bbb);
+    return aaa + bbb;
+  }
+}
