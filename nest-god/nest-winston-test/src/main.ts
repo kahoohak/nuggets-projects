943dc6ddc420e22a0cbe529225a07d0d0a7f3b02
zhaoxingchen <zhaoxingchen@comen.com>
2023-12-05 21:03:25 +0800

    nest: Nest 集成日志框架 Winston

---
+++ nest-god/nest-winston-test/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MyLogger } from './MyLogger';
+import { WINSTON_LOGGER_TOKEN } from './winston.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(WINSTON_LOGGER_TOKEN));
+
+  await app.listen(3000);
+}
+bootstrap();
