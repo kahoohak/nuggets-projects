99cd1c5782c408cfea5aa0fb54bbe243bfaf74cf
zhaoxingchen <zhaoxingchen@comen.com>
2024-02-29 20:56:03 +0800

    大文件上传合并测试

--- nest-god/large-file-sharding-upload/src/app.controller.ts
+++ nest-god/large-file-sharding-upload/src/app.controller.ts
@@ -3,11 +3,13 @@ import {
   Controller,
   Get,
   Post,
+  Query,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import * as fs from 'node:fs';
 
 @Controller()
 export class AppController {
@@ -31,11 +33,28 @@ export class AppController {
     console.log('files', files);
     console.log('body', body);
 
-    const { name } = body;
+    const { name, index } = body;
     const chunkDir = 'uploads/chunk_' + name;
 
     if (!fs.existsSync(chunkDir)) {
       fs.mkdirSync(chunkDir);
     }
+
+    fs.cpSync(files[0].path, `${chunkDir}/${name}-${index}`);
+    fs.rmSync(files[0].path);
+  }
+
+  @Get('merge')
+  merge(@Query('name') name: string) {
+    const chunkDir = 'uploads/chunk_' + name;
+    const files = fs.readdirSync(chunkDir);
+
+    let start = 0;
+    files.map((file) => {
+      const filePath = chunkDir + '/' + file;
+      const stream = fs.createReadStream(filePath);
+      stream.pipe(fs.createWriteStream('uploads/' + name, { start }));
+      start += fs.statSync(filePath).size;
+    });
   }
 }
